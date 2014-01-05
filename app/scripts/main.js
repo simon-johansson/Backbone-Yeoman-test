00da69141cdab8352c3da195793a657734e3008d
Simon Johansson <mail@simon-johansson.com>
2014-01-05 21:24:24 +0100

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global bbTest, $*/
+
+
+window.bbTest = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    bbTest.init();
+});
